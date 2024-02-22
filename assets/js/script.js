6bdb3d6fc7f95f12923d3b80e8aa32a40c4d67ca
Noah Sehman <noah.sehman@gmail.com>
2024-02-21 19:33:24 -0700

    idk man something about styles and scripts and websites. you're welcome

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
-import Chart from 'chart.js/auto';
-
-
 const gsect = document.getElementById("sects");
 const graphEL = gsect.children[0];
 const freqEL = gsect.children[1];
+const lidaa = [];
+
+for (let i = 0; i < freqEL.children[0].children.length; i++) {
+    lidaa.push(freqEL.children[0].children[i]);
+}
 
